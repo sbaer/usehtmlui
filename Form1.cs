d771c0e7f23e2dc15a2c3c1f25cd7dd648d384be
sbaer <steve@mcneel.com>
2011-08-10 21:24:11 -0700

    initial add

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UseHtmlUi
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      var controller = new MyController(textBox1);
+      var stream = this.GetType().Assembly.GetManifestResourceStream("UseHtmlUi.HTMLPage1.htm");
+      SdkFunctions.ShowHtmlDialog(stream, controller);
+    }
+  }
+
+  class MyController : BrowserController
+  {
+    System.Windows.Forms.TextBox m_stringbox;
+    public MyController(System.Windows.Forms.TextBox stringbox)
+    {
+      m_stringbox = stringbox;
+    }
+
+    public override string Callback1(string arg)
+    {
+      m_stringbox.Text = m_stringbox.Text + arg + "\r\n";
+      int iVal = int.Parse(arg);
+      return (iVal * 2).ToString();
+    }
+
+    public override string Callback2(string arg1, string arg2)
+    {
+      if (arg1.Equals("show"))
+      {
+        m_stringbox.Text = m_stringbox.Text + arg1 + " : " + arg2 + "\r\n";
+        System.Windows.Forms.MessageBox.Show(arg2);
+        InvokeScript("setText", "Callback2 was called");
+      }
+      return string.Empty;
+    }
+  }
+}
---
+++ SdkFunctions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UseHtmlUi
+{
+  static class SdkFunctions
+  {
+    public static void ShowHtmlDialog(System.IO.Stream htmlStream, BrowserController controller)
+    {
+      System.Windows.Forms.Form f = new System.Windows.Forms.Form();
+      System.Windows.Forms.WebBrowser browser = new System.Windows.Forms.WebBrowser();
+      f.Controls.Add(browser);
+      browser.Dock = System.Windows.Forms.DockStyle.Fill;
+      BrowserGlue glue = new BrowserGlue(browser, controller);
+      browser.ObjectForScripting = glue;
+      browser.DocumentStream = htmlStream;
+      f.ShowDialog();
+    }
+  }
+
+
+  public class BrowserController
+  {
+    BrowserGlue m_glue;
+    internal void SetGlue(BrowserGlue glue)
+    {
+      m_glue = glue;
+    }
+
+    public void InvokeScript(string functionName)
+    {
+      m_glue.InvokeScript(functionName);
+    }
+
+    public void InvokeScript(string functionName, string arg)
+    {
+      m_glue.InvokeScript(functionName, arg);
+   }
+
+    public void InvokeScript(string functionName, string arg1, string arg2)
+    {
+      m_glue.InvokeScript(functionName, arg1, arg2);
+    }
+
+    public virtual string Callback()
+    {
+      return string.Empty;
+    }
+
+    public virtual string Callback1(string arg)
+    {
+      return string.Empty;
+    }
+
+    public virtual string Callback2(string arg1, string arg2)
+    {
+      return string.Empty;
+    }
+  }
+
+  /// <summary>
+  /// Used internally to bind a WebBrowser to a BrowserController class
+  /// </summary>
+  [System.Runtime.InteropServices.ComVisible(true)]
+  public class BrowserGlue
+  {
+    System.Windows.Forms.WebBrowser m_browser;
+    BrowserController m_controller;
+    internal BrowserGlue(System.Windows.Forms.WebBrowser browser, BrowserController controller)
+    {
+      m_browser = browser;
+      m_controller = controller;
+      m_controller.SetGlue(this);
+    }
+
+    public void InvokeScript(string functionName)
+    {
+      m_browser.Document.InvokeScript(functionName);
+    }
+
+    public void InvokeScript(string functionName, string arg)
+    {
+      m_browser.Document.InvokeScript(functionName, new object[] { arg });
+    }
+
+    public void InvokeScript(string functionName, string arg1, string arg2)
+    {
+      m_browser.Document.InvokeScript(functionName, new object[] { arg1, arg2 });
+    }
+
+    public virtual string Callback()
+    {
+      return m_controller.Callback();
+    }
+
+    public virtual string Callback1(string arg)
+    {
+      return m_controller.Callback1(arg);
+    }
+
+    public virtual string Callback2(string arg1, string arg2)
+    {
+      return m_controller.Callback2(arg1, arg2);
+    }
+  }
+}
